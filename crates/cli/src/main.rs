49efd95eff5c1519b03bc185b27d2a32a5b48aff
calmkoi <brayden.fisher28@gmail.com>
2025-04-04 23:26:44 +1030

    feat(cli): add basic CLI with hardcoded Gen 1 example

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,3 +1,16 @@
+use engine::{Pokemon, Move, MoveCategory, Type, Stats, calculate_gen1};
+
 fn main() {
-    println!("Hello, world!");
+    let pikachu = Pokemon { 
+        name: "Pikachu".into(), 
+        types: [Type::Electric, Type::None], 
+        stats: Stats {lvl: 100, hp: 35, attack: 55, defense: 30, special: 50, speed: 90}, 
+    };
+    let thunderbolt = Move { 
+        name: "Thunderbolt".into(),
+        typ: Type::Electric,
+        power: 95,
+        category: MoveCategory::Special,
+    };
+    println!("Damage: {}", calculate_gen1(&pikachu, &pikachu, &thunderbolt));
 }
