94896b66e9e6101e31accb21b206a31d7957f7f6
calmkoi <brayden.fisher28@gmail.com>
2025-04-04 22:50:08 +1030

    fix(engine): add Type and Stats to re-export

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -3,6 +3,6 @@ pub mod poke_move;
 pub mod damage;
 
 // Re-export for easy external use
-pub use pokemon::Pokemon;
+pub use pokemon::{Pokemon, Type, Stats};
 pub use poke_move::{Move, MoveCategory};
 pub use damage::calculate_gen1;
