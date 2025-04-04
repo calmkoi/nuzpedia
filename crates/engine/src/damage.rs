9879ef1413bf651e8902c1738791c8cb37e1d2a0
calmkoi <brayden.fisher28@gmail.com>
2025-04-04 23:05:45 +1030

    fix(engine): upcast values in calculate_gen1 to u32 to prevent overflow

--- crates/engine/src/damage.rs
+++ crates/engine/src/damage.rs
@@ -6,14 +6,25 @@ pub fn calculate_gen1(
     mov: &Move,
 ) -> u16 {
     // Determine STAB
-    let stab = if attacker.types.contains(&mov.typ) {1.5} else {1.0};
+    let stab = if attacker.types.contains(&mov.typ) { 1.5 } else { 1.0 };
 
     // TODO: logic to handle status moves
     // Use Attack or Special based on move type
-    let attacker_attack = if mov.category == MoveCategory::Physical {attacker.stats.attack} else {attacker.stats.special};
+    let attacker_attack = if mov.category == MoveCategory::Physical {
+        attacker.stats.attack
+    } else {
+        attacker.stats.special
+    };
+
+    // Convert values to u32 before multiplication
+    let lvl = attacker.stats.lvl as u32;
+    let power = mov.power as u32;
+    let attack = attacker_attack as u32;
+    let defense = defender.stats.defense as u32;
 
     // Base damage formula (excl. crit and random)
-    let base = ((2 * attacker.stats.lvl / 5 + 2) * mov.power * attacker_attack / defender.stats.defense) / 50 + 2;
+    let base = (((2 * lvl / 5 + 2) * power * attack) / (defense * 50) + 2) as u32;
+
 
     (base as f64 * stab) as u16
 }
@@ -23,11 +34,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_gn1_damage() {
+    fn test_gen1_damage() {
         let pikachu = Pokemon {
             name: "Pikachu".into(),
             types: [Type::Electric, Type::None],
-            stats: Stats {lvl: 50, hp: 35, attack: 55, defense: 30, special: 50, speed: 90},
+            stats: Stats {lvl: 100, hp: 35, attack: 55, defense: 30, special: 50, speed: 90},
         };
         let thunderbolt = Move {
             name: "Thunderbolt".into(),
@@ -35,6 +46,6 @@ mod tests {
             power: 95,
             category: MoveCategory::Special,
         };
-        assert_eq!(calculate_gen1(&pikachu, &pikachu, &thunderbolt), 42); // Example value
+        assert_eq!(calculate_gen1(&pikachu, &pikachu, &thunderbolt), 202); // Example value
     }
 }
