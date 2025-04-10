648f93ca7be631cdd16fec9ec0ed06bdb594b1ba
calmkoi <brayden.fisher28@gmail.com>
2025-04-10 20:26:29 +0930

    feat(engine): expose DamageRoll though engine interface
    Made DamageRoll enum publicly accessible from lib.rs

--- crates/engine/src/lib.rs
+++ crates/engine/src/lib.rs
@@ -6,5 +6,5 @@ pub mod types;
 // Re-export for easy external use
 pub use pokemon::{PokemonGen1, StatsGen1, StatStagesGen1, StatusGen1};
 pub use poke_move::{MoveGen1, MoveCategory};
-pub use damage::calc_damage_gen_1;
+pub use damage::{DamageRoll, calc_damage_gen_1};
 pub use types::{TypeGen1, type_effectiveness_gen_1, type_effectiveness_gen_1_fast};
